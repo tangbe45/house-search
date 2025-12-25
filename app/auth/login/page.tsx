1eb1175f4ee346e3aa3d8f7a2b0738a66930e470
tangbe lucien <lucientangbe45@gmail.com>
2025-12-25 07:22:37 +0100

    feat: add login and signup pages should redirect users to dashboard if they are already loged in

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,6 +1,15 @@
 import { LoginForm } from "@/components/forms/login-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
--- app/auth/sign-up/page.tsx
+++ app/auth/sign-up/page.tsx
@@ -1,6 +1,15 @@
 import { SignupForm } from "@/components/forms/signup-form";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth.api.getSession({ headers: await headers() });
+
+  if (session) {
+    redirect("/dashboard");
+  }
+
   return (
     <div className="flex min-h-svh w-full items-center justify-center p-2 sm:p-6 md:p-10">
       <div className="w-full max-w-sm">
