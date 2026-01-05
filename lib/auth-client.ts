f68994c4094e481611b17867574731c43409d4c1
tangbe lucien <lucientangbe45@gmail.com>
2026-01-05 13:22:59 +0100

    fix: import dotenv config to auth client

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,7 @@
 import { createAuthClient } from "better-auth/react";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { auth } from "./auth";
+import "dotenv/config";
 
 export const authClient = createAuthClient({
   baseURL: process.env.BETTER_AUTH_URL,
