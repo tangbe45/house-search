9a459efa553948fd7eebf390f6acb76e23748f1a
tangbe lucien <lucientangbe45@gmail.com>
2026-01-05 13:10:29 +0100

    fix: Oauth redirecting to localhost

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,6 +3,6 @@ import { inferAdditionalFields } from "better-auth/client/plugins";
 import { auth } from "./auth";
 
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.BETTER_AUTH_URL,
   plugins: [inferAdditionalFields<typeof auth>()],
 });
